1345d0d98b08696fdedca2f18bf1c94dbea71b41
Gonçalo Figueiredo <Gonalo Figueiredo@WIN-GESFLHLR4EE>
2019-10-01 16:45:55 +0100

    Adicionar arquivos de projeto.

---
+++ Jogo Da Memoria 7/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.p1 = New System.Windows.Forms.PictureBox()
+        Me.p2 = New System.Windows.Forms.PictureBox()
+        Me.p3 = New System.Windows.Forms.PictureBox()
+        Me.p4 = New System.Windows.Forms.PictureBox()
+        Me.p5 = New System.Windows.Forms.PictureBox()
+        Me.p6 = New System.Windows.Forms.PictureBox()
+        Me.p7 = New System.Windows.Forms.PictureBox()
+        Me.p8 = New System.Windows.Forms.PictureBox()
+        Me.p9 = New System.Windows.Forms.PictureBox()
+        Me.p10 = New System.Windows.Forms.PictureBox()
+        Me.p11 = New System.Windows.Forms.PictureBox()
+        Me.p12 = New System.Windows.Forms.PictureBox()
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p12, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.DarkRed
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Location = New System.Drawing.Point(278, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(265, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Jogo da Memória"
+        '
+        'p1
+        '
+        Me.p1.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Chelsea
+        Me.p1.Location = New System.Drawing.Point(129, 94)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(128, 128)
+        Me.p1.TabIndex = 1
+        Me.p1.TabStop = False
+        '
+        'p2
+        '
+        Me.p2.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.ManU
+        Me.p2.Location = New System.Drawing.Point(263, 94)
+        Me.p2.Name = "p2"
+        Me.p2.Size = New System.Drawing.Size(128, 128)
+        Me.p2.TabIndex = 2
+        Me.p2.TabStop = False
+        '
+        'p3
+        '
+        Me.p3.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Chelsea
+        Me.p3.Location = New System.Drawing.Point(397, 94)
+        Me.p3.Name = "p3"
+        Me.p3.Size = New System.Drawing.Size(128, 128)
+        Me.p3.TabIndex = 3
+        Me.p3.TabStop = False
+        '
+        'p4
+        '
+        Me.p4.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Spurs
+        Me.p4.Location = New System.Drawing.Point(531, 94)
+        Me.p4.Name = "p4"
+        Me.p4.Size = New System.Drawing.Size(128, 128)
+        Me.p4.TabIndex = 4
+        Me.p4.TabStop = False
+        '
+        'p5
+        '
+        Me.p5.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.ManCity
+        Me.p5.Location = New System.Drawing.Point(129, 228)
+        Me.p5.Name = "p5"
+        Me.p5.Size = New System.Drawing.Size(128, 128)
+        Me.p5.TabIndex = 5
+        Me.p5.TabStop = False
+        '
+        'p6
+        '
+        Me.p6.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Liverpool
+        Me.p6.Location = New System.Drawing.Point(263, 228)
+        Me.p6.Name = "p6"
+        Me.p6.Size = New System.Drawing.Size(128, 128)
+        Me.p6.TabIndex = 6
+        Me.p6.TabStop = False
+        '
+        'p7
+        '
+        Me.p7.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Wolves
+        Me.p7.Location = New System.Drawing.Point(397, 228)
+        Me.p7.Name = "p7"
+        Me.p7.Size = New System.Drawing.Size(128, 128)
+        Me.p7.TabIndex = 7
+        Me.p7.TabStop = False
+        '
+        'p8
+        '
+        Me.p8.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Wolves
+        Me.p8.Location = New System.Drawing.Point(531, 228)
+        Me.p8.Name = "p8"
+        Me.p8.Size = New System.Drawing.Size(128, 128)
+        Me.p8.TabIndex = 8
+        Me.p8.TabStop = False
+        '
+        'p9
+        '
+        Me.p9.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.ManU
+        Me.p9.Location = New System.Drawing.Point(129, 362)
+        Me.p9.Name = "p9"
+        Me.p9.Size = New System.Drawing.Size(128, 128)
+        Me.p9.TabIndex = 9
+        Me.p9.TabStop = False
+        '
+        'p10
+        '
+        Me.p10.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Spurs
+        Me.p10.Location = New System.Drawing.Point(263, 362)
+        Me.p10.Name = "p10"
+        Me.p10.Size = New System.Drawing.Size(128, 128)
+        Me.p10.TabIndex = 10
+        Me.p10.TabStop = False
+        '
+        'p11
+        '
+        Me.p11.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.Liverpool
+        Me.p11.Location = New System.Drawing.Point(397, 362)
+        Me.p11.Name = "p11"
+        Me.p11.Size = New System.Drawing.Size(128, 128)
+        Me.p11.TabIndex = 11
+        Me.p11.TabStop = False
+        '
+        'p12
+        '
+        Me.p12.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.ManCity
+        Me.p12.Location = New System.Drawing.Point(531, 362)
+        Me.p12.Name = "p12"
+        Me.p12.Size = New System.Drawing.Size(128, 128)
+        Me.p12.TabIndex = 12
+        Me.p12.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Jogo_Da_Memoria_7.My.Resources.Resources.sam2
+        Me.ClientSize = New System.Drawing.Size(836, 560)
+        Me.Controls.Add(Me.p12)
+        Me.Controls.Add(Me.p11)
+        Me.Controls.Add(Me.p10)
+        Me.Controls.Add(Me.p9)
+        Me.Controls.Add(Me.p8)
+        Me.Controls.Add(Me.p7)
+        Me.Controls.Add(Me.p6)
+        Me.Controls.Add(Me.p5)
+        Me.Controls.Add(Me.p4)
+        Me.Controls.Add(Me.p3)
+        Me.Controls.Add(Me.p2)
+        Me.Controls.Add(Me.p1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p12, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents p1 As PictureBox
+    Friend WithEvents p2 As PictureBox
+    Friend WithEvents p3 As PictureBox
+    Friend WithEvents p4 As PictureBox
+    Friend WithEvents p5 As PictureBox
+    Friend WithEvents p6 As PictureBox
+    Friend WithEvents p7 As PictureBox
+    Friend WithEvents p8 As PictureBox
+    Friend WithEvents p9 As PictureBox
+    Friend WithEvents p10 As PictureBox
+    Friend WithEvents p11 As PictureBox
+    Friend WithEvents p12 As PictureBox
+End Class
---
+++ Jogo Da Memoria 7/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Clicar(sender As Object, e As EventArgs) Handles p1.Click, p2.Click, p3.Click, p4.Click, p5.Click, p6.Click, p7.Click, p8.Click, p9.Click, p10.Click, p11.Click, p12.Click
+        Dim fig As New PictureBox
+        Dim i = Int(Rnd() * 6)
+        fig.Image = My.Resources.Chelsea
+
+        Select Case i
+            Case 0 : fig.Image = My.Resources.Chelsea
+            Case 1 : fig.Image = My.Resources.ManU
+            Case 2 : fig.Image = My.Resources.Liverpool
+            Case 3 : fig.Image = My.Resources.ManCity
+            Case 4 : fig.Image = My.Resources.Wolves
+            Case 5 : fig.Image = My.Resources.Spurs
+        End Select
+
+        Select Case sender.name
+            Case "p1" : p1.BackgroundImage = fig.Image
+            Case "p2" : p2.BackgroundImage = fig.Image
+            Case "p3" : p3.BackgroundImage = fig.Image
+            Case "p4" : p4.BackgroundImage = fig.Image
+            Case "p5" : p5.BackgroundImage = fig.Image
+            Case "p6" : p6.BackgroundImage = fig.Image
+            Case "p7" : p7.BackgroundImage = fig.Image
+            Case "p8" : p8.BackgroundImage = fig.Image
+            Case "p9" : p9.BackgroundImage = fig.Image
+            Case "p10" : p10.BackgroundImage = fig.Image
+            Case "p11" : p11.BackgroundImage = fig.Image
+            Case "p12" : p12.BackgroundImage = fig.Image
+        End Select
+
+    End Sub
+End Class
